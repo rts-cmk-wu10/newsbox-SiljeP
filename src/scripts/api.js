0b54e39ebfa266bc4097cde817d515c22a09beb5
SiljeP <u2440913@rts-365.dk>
2023-11-08 15:39:34 +0100

    small fixes

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -2,11 +2,17 @@ export default (function () {
 
     const KEY = "X3CCZda4H2e4bv2yUieN2AI5m0U7njTV"
     const HEALTH = document.querySelector(".health")
+    const CATEGORY_DISPLAY_ICON = document.querySelectorAll(".categories__displayIcon")
 
-    const LINK_CONTAINER = document.createElement("div")
-    LINK_CONTAINER.classList.add("health__articleTextContainer")
-    LINK_CONTAINER.style.display = "none"
-    
+//     CATEGORY_DISPLAY_ICON.forEach(element => {
+//         element.addEventListener("click", iconClick)
+//       })
+       
+//    function iconClick() {
+//         CATEGORY_DISPLAY_ICON.classList.toggle("fa-chevron-right")
+//         CATEGORY_DISPLAY_ICON.classList.toggle("fa-chevron-down")
+    // }
+
     fetch(`https://api.nytimes.com/svc/topstories/v2/health.json?api-key=${KEY}`)
         .then(function (response) {
             if (response.status !== 200)
@@ -23,22 +29,25 @@ export default (function () {
 
                     const ARTICLE = document.createElement("article")
 
+                  
                     ARTICLE.classList.add("health__article")
                     ARTICLE.innerHTML = `<img class="health__articleImage" src="${object.multimedia[2].url}">
        <div class="health__articleTextContainer">
        <h1 class="health__articleTitle">${object.title}</h1>
-       <p class="health__articleText">${object.abstract}</p></div>
-       `
+       <p class="health__articleText">${object.abstract}</p>
+       <div class="health__articleLinkContainer"></div></div>`
 
                     HEALTH.append(ARTICLE)
+                    const LINK_CONTAINER = ARTICLE.querySelector(".health__articleLinkContainer")
 
                     ARTICLE.addEventListener("click", clickHandler)
 
                     function clickHandler() {
-                        LINK_CONTAINER.style.display =  "block"
+                        LINK_CONTAINER.style.display = "block"
                         LINK_CONTAINER.innerHTML = `<a class="health__articleLink" target="_blank" href="${object.url}">Go to the article</a>`
-                        ARTICLE.append(LINK_CONTAINER)
-                    }
+                    }  
+                   
+                  
                 }
                 // console.log(data);
 
